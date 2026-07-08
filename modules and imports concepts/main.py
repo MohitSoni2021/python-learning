0906e7670b09ef5a3d4c032ffc7dc411257cbbf2
Mohit Soni <mohitsoni2006ms@gmail.com>
2026-07-08 12:00:00 +0530

    feat(modules): add modules and imports execution scripts

---
+++ modules and imports concepts/main.py
@@ -0,0 +1,12 @@
+import math_utils as MU
+
+from TestModule.mathsfunctions import mathAdd as MA
+from TestModule.corefile import level1filefunction, level2filefunction
+
+a = 10
+b = 20
+
+print(MA(a, b))
+
+level2filefunction()
+level1filefunction()
